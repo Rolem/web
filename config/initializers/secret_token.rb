79b26637debdb522ab91e0a25945b41112d35808
David Florez <dflorez6@gmail.com>
2013-04-30 23:27:35 -0500

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RolemWeb::Application.config.secret_token = '878b1a8df643bf7eee76344c55878a1b45866ead4cf3602222fed0d9578136de35add4c86d4aa01d2cdd730324e9ac5dcc6f96e35e772dca4c0c44e6efd5b73f'
