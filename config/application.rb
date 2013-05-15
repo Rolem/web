871dda5aef914a39b3c81136c86f6b9968a10cf1
Juan Sebastián Rey Sánchez <batianusrey@gmail.com>
2013-05-15 17:28:22 -0500

    Trying one new change.

--- config/application.rb
+++ config/application.rb
@@ -30,8 +30,7 @@ module RolemWeb
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     config.i18n.default_locale = :'es'
     config.i18n.locale = :'es'
 
