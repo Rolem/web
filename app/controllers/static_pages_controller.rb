bb0c24e6f7a4f83ed1c69950dfba703805525b80
Andres F. Lopez <ing.ratosocial@gmail.com>
2014-04-16 23:17:46 -0500

    submit_action_review

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,11 @@
 class StaticPagesController < ApplicationController
-
+  def submit_contact
+        @user_capture = ContactInformation.new full_name: params[:full_name], email: params[:email], message: params[:message]
+    if @user_capture.save
+      flash.now[:success] = 'Gracias por contarnos tu opinion, estaremos en contacto pronto.'
+    else
+      flash.now[:error] = 'Ups, algo pasa. Por favor prueba llenando el formulario nuevamente.'
+    end
+    render 'index'
+  end
 end
