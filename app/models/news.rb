db7e3205791031c11559c0224d9f6152b41529c9
Andres F. Lopez <ing.ratosocial@gmail.com>
2014-04-28 05:36:30 -0500

    raven

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,7 +1,6 @@
 class News < ActiveRecord::Base
 	
   attr_accessible :description, :image_url, :tags, :title
-  attr_accessible :description, :image_url, :title
   validates :title, :description, :image_url, presence: true
 # 
   validates :title, uniqueness: true
